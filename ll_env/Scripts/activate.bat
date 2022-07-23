5a9368d3878dce339e34d9547d52e59a83591c6d
Yonghang Wu <wuyonghang12138@gmail.com>
2022-07-24 00:57:10 +0800

    finished

--- ll_env/Scripts/activate.bat
+++ ll_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\CODE\python_workspace\django_web\ll_env
+set VIRTUAL_ENV=D:\CODE\python_workspace\网络s1901_吴永航_1911660114\django_web\ll_env
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,6 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(ll_env) 
 
 :END
 if defined _OLD_CODEPAGE (
